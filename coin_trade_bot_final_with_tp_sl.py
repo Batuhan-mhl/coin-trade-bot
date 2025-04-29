b891cf01c6cffa7316dcc4e313f989c1d408bfdb
Batuhan-mhl <batuhanmhl@gmail.com>
2025-04-29 03:50:20 +0300

    Delete coin_trade_bot_final_with_tp_sl.py

--- coin_trade_bot_final_with_tp_sl.py
+++
@@ -1,4 +0,0 @@
-# Günlük trade fırsat analizi için sahte veri simülasyonu yapacağız.
-def analyze_tp_sl():
-    from telegram_utils import send_telegram_message
-    send_telegram_message("TP/SL analiz raporu: Şu anda veri bulunamadı.")
