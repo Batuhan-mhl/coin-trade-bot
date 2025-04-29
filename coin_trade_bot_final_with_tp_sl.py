7710825cfde99fdf4035d049be771b8e644051d3
Batuhan-mhl <batuhanmhl@gmail.com>
2025-04-29 03:52:30 +0300

    Add files via upload

---
+++ coin_trade_bot_final_with_tp_sl.py
@@ -0,0 +1,4 @@
+# Günlük trade fırsat analizi için sahte veri simülasyonu yapacağız.
+def analyze_tp_sl():
+    from telegram_utils import send_telegram_message
+    send_telegram_message("TP/SL analiz raporu: Şu anda veri bulunamadı.")
